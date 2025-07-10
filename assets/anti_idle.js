164526723520691a3f84a0045fff0bcc87e51b8c
Andres Machuca <123473293+AndresMachuca21@users.noreply.github.com>
2025-07-10 12:23:14 -0500

    Update anti_idle.js

--- assets/anti_idle.js
+++ assets/anti_idle.js
@@ -1,3 +1,4 @@
+// Simula movimiento del mouse cada 15 minutos
 setInterval(() => {
     const event = new MouseEvent("mousemove", {
         bubbles: true,
@@ -6,4 +7,14 @@ setInterval(() => {
         clientY: 0
     });
     document.dispatchEvent(event);
-}, 1800000); // cada 30 minutos
+}, 900000); // 15 minutos en milisegundos
+
+// Simula una pulsación de tecla invisible cada 15 minutos
+setInterval(() => {
+    const event = new KeyboardEvent("keydown", {
+        bubbles: true,
+        cancelable: true,
+        key: "Shift"
+    });
+    document.dispatchEvent(event);
+}, 900000); // 15 minutos en milisegundos
