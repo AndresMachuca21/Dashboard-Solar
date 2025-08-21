fd8bcef8b8d937768cb571c05b64f63fdb864cf5
Andres Machuca <123473293+AndresMachuca21@users.noreply.github.com>
2025-08-21 15:57:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -253,7 +253,7 @@ app.layout = html.Div(
                 html.H4("Total combinado", style={"fontSize": "18px", "color": "#000000", "marginBottom": "5px"}),
                 html.P(id="kpi-total", style={"fontSize": "28px", "color": "#000000", "marginTop": "5px", "left": "-20px"})
             ], style={"textAlign": "center", "flex": "1"}),
-        ], style={"display": "flex", "gap": "4px", "marginTop": "0px", "marginBottom": "0px", "maxWidth": "100px"}),
+        ], style={"textAlign": "center", "display": "flex", "gap": "4px", "marginTop": "0px", "marginBottom": "0px", "maxWidth": "100px"}),
 
         # Última actualización
         html.Div(id="ultima-actualizacion", style={
